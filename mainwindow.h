2087a93b0ad24a65e92753096160a475e7fa2697
HolyPush <1930127052@qq.com>
2024-07-06 16:42:06 +0800

    StartWindow

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QLabel>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -13,11 +16,34 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+        /*
+         * The function paintEvent is to load all the components on the mainwindow.
+         * It includes the load of labels, lineedits and buttons
+         */
+        void paintEvent(QPaintEvent *event);
+
+        /*
+         * The function expandTextbox is to expand the width of a lineedit textbox
+         * when the number of characters exceeds the maximum capacity.
+         */
+        void expandTextbox(const QString &text);
+
+        /*
+         * The function setNickname is used to show a message box where player should enter his/her nickname.
+         */
+        void setNickname();
+
+        /*
+         * The function setServerAddress is used to show a message box where player should enter his/her server address.
+         */
+        void setServerAddress();
+
+    private:
+        Ui::MainWindow *ui;
+        int Nickname_Input_Width = 200;
 };
 #endif // MAINWINDOW_H
