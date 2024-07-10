7a444088e6b39def8a34ab8daee4aa738eb64424
wzsCrossing <1076547517@qq.com>
2024-07-10 16:38:27 +0800

    add App layer

--- src/app/GeneralsApp.cpp
+++ src/app/GeneralsApp.cpp
@@ -1,12 +1,15 @@
 #include "GeneralsApp.h"
 #include <memory>
+#include <QSharedMemory>
 
 GeneralsApp::GeneralsApp() {
     std::shared_ptr<GeneralsGameModel> model(new GeneralsGameModel);
     std::shared_ptr<GeneralsViewModel> viewModel(new GeneralsViewModel);
     viewModel->setModel(model);
 
-    mainWindow = new MainWindow(nullptr, viewModel);
+    GeneralsViewModel* ptr = viewModel.get();
+    QSharedPointer<GeneralsViewModel> sharedViewModel(ptr);
+    mainWindow = new MainWindow(nullptr, sharedViewModel);
 }
 
 void GeneralsApp::show() const {
